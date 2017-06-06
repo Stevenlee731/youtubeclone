dfe4faf86bebabcb239fd6d18ea28f7514e3ecea
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-05 17:57:56 -0700

    rendered video to dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 
-class App extends React.Component {
+const API_KEY = 'AIzaSyChIxSUe9wKvSHX7pHsPJDacc1I3GuRV6A'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        hello
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'))
